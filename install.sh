ed95950feaafc9222e2aa9d252b5ee0a2bc1ec09
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2016-03-26 12:19:04 +0100

    Change: skwp url to lfilho's

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    echo "Installing lfilho's YADR for the first time"
+    git clone --depth=1 https://github.com/lfilho/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
