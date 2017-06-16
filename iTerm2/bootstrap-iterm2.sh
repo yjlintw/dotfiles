96f3ddc3fff259f8acc20da7436d57eef336fc99
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2017-06-16 12:05:55 +0200

    [iterm] Auto setup iTerm to load prefs from custom folder

--- iTerm2/bootstrap-iterm2.sh
+++ iTerm2/bootstrap-iterm2.sh
@@ -3,9 +3,10 @@
 # Make iTerm understand italics
 tic ~/.yadr/iTerm2/xterm-256color.terminfo
 
-# Copy plist file from here to the proper location
-mv ~/Library/Preferences/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist.backup
-ln -nfs ~/.yadr/iTerm2/com.googlecode.iterm2.plist ~/Library/Preferences/
+# Specify the preferences directory
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.yadr/iTerm2"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
 # Add shortcut to Dock
 defaults write com.apple.dock persistent-apps -array-add "<dict><key>tile-data</key><dict><key>file-data</key><dict><key>_CFURLString</key><string>/Applications/iTerm.app</string><key>_CFURLStringType</key><integer>0</integer></dict></dict></dict>"
