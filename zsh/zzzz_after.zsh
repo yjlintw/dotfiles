4367ab986e5636478d718abac63bba9b4849ae26
Luiz Gonzaga dos Santos Filho <lfilho@gmail.com>
2017-07-02 03:30:36 +0200

    [tmuxp] Remove forgotten completion (not working yet)

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -1,8 +1,3 @@
-# Completions for tmuxp:
-autoload bashcompinit
-bashcompinit
-eval "$(_TMUXP_COMPLETE=source tmuxp)"
-
 # Load any custom after code
 if [ -d $HOME/.zsh.after/ ]; then
   if [ "$(ls -A $HOME/.zsh.after/)" ]; then
